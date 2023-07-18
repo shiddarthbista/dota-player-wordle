9b29b650c1bd3c1167d34f6ef8a9d75c194a8bac
shiddarthbista <61994769+shiddarthbista@users.noreply.github.com>
2023-07-18 17:08:25 -0400

    impove for mobile

--- src/components/SelectedPlayerDetails.jsx
+++ src/components/SelectedPlayerDetails.jsx
@@ -19,7 +19,7 @@ export default function SelectedPlayerDetails({
     <div className="selectedPlayer-details">
       <ToastContainer
           position="top-center"
-          autoClose={5000}
+          autoClose={3000}
           hideProgressBar={false}
           newestOnTop
           closeOnClick

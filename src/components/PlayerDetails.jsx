fb65dd49f5c9694bcb4adacc9bdfaa1960bc0de1
shiddarthbista <61994769+shiddarthbista@users.noreply.github.com>
2023-07-17 23:14:45 -0400

    Add country

--- src/components/PlayerDetails.jsx
+++ src/components/PlayerDetails.jsx
@@ -1,25 +1,30 @@
 import "./PlayerDetails.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
+import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
 
 export default function selectedPlayerDetails({
   selectedPlayer,
   randomPlayer,
 }) {
   const roleImageSrc = `src/assets/${selectedPlayer.position}.png`;
+  const countryImageSrc = `src/assets/country/${selectedPlayer.country}.png`;
   console.log(selectedPlayer);
   console.log(randomPlayer);
-
 
   return (
     <div className="selectedPlayer-details">
       <div className="selectedPlayer-name">{selectedPlayer.name}</div>
       <div className="circles-container">
-        <div className="circle">{selectedPlayer.age}</div>
-        <div className="circle" style={{ backgroundColor: selectedPlayer.position === randomPlayer.position ? "green" : "red" }}>
+        <div className="circle-age" style={{backgroundColor:selectedPlayer.age === randomPlayer.age ? "green" : "red"}}>
+          {selectedPlayer.age}{" "}
+          {randomPlayer.age > selectedPlayer.age ? (<FontAwesomeIcon icon={faArrowUp} />) : randomPlayer.age < selectedPlayer.age ? (<FontAwesomeIcon icon={faArrowDown} />) : null}
+        </div>
+        <div className="circle" style={{backgroundColor:selectedPlayer.position === randomPlayer.position? "green": "red",}} >
           <img src={roleImageSrc} alt={selectedPlayer.role}></img>
         </div>
-        <div className="circle">
-          {selectedPlayer.country}
-          {/* <img src={selectedPlayer.countryFlag} alt="Country" /> */}
+        <div className="circle" style={{backgroundColor:selectedPlayer.country === randomPlayer.country? "green": "red",}}>
+        <img src={countryImageSrc} alt={selectedPlayer.country}></img>
         </div>
         <div className="circle">
           {selectedPlayer.team}

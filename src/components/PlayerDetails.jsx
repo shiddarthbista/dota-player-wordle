a9a55f6be184b70fb898753440bc71d449d846c7
shiddarthbista <61994769+shiddarthbista@users.noreply.github.com>
2023-07-18 01:44:46 -0400

    regions

--- src/components/PlayerDetails.jsx
+++ src/components/PlayerDetails.jsx
@@ -10,6 +10,7 @@ export default function selectedPlayerDetails({
   const roleImageSrc = `src/assets/${selectedPlayer.position}.png`;
   const countryImageSrc = `src/assets/country/${selectedPlayer.country}.png`;
   const teamImageSrc = `src/assets/team/${selectedPlayer.team}.png`;
+  const regionImageSrc = `src/assets/region/${selectedPlayer.region}.png`;
   console.log(selectedPlayer);
   console.log(randomPlayer);
 
@@ -22,18 +23,16 @@ export default function selectedPlayerDetails({
           {randomPlayer.age > selectedPlayer.age ? (<FontAwesomeIcon icon={faArrowUp} />) : randomPlayer.age < selectedPlayer.age ? (<FontAwesomeIcon icon={faArrowDown} />) : null}
         </div>
         <div className="circle" style={{backgroundColor:selectedPlayer.position === randomPlayer.position? "green": "red",}} >
-          <img src={roleImageSrc} alt={selectedPlayer.role}></img>
+            <img src={roleImageSrc} alt={selectedPlayer.role}></img>
         </div>
         <div className="circle" style={{backgroundColor:selectedPlayer.country === randomPlayer.country? "green": "red",}}>
-        <img src={countryImageSrc} alt={selectedPlayer.country}></img>
+            <img src={countryImageSrc} alt={selectedPlayer.country} style={{ border: "2px solid #222" }}></img>
         </div>
         <div className="circle" style={{backgroundColor:selectedPlayer.team === randomPlayer.team? "green": "red",}}>
-        <img src={teamImageSrc} alt={selectedPlayer.team}></img>
-          {/* <img src={selectedPlayer.teamLogo} alt="Team" /> */}
+            <img src={teamImageSrc} alt={selectedPlayer.team}></img>
         </div>
-        <div className="circle">
-          {selectedPlayer.region}
-          {/* <img src={selectedPlayer.regionLogo} alt="Region" /> */}
+        <div className="circle" style={{backgroundColor:selectedPlayer.region === randomPlayer.region? "green": "red"}}>
+            <img src={regionImageSrc} alt={selectedPlayer.region} style={{ border: "2px solid black" , borderRadius: "50%"}}></img>
         </div>
       </div>
     </div>
